fa5995ebb101f34e4bb3cb6446504501c004ce24
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-05 10:26:59 -0400

    change attention module to take batch of tokens as input in forward

--- trans.py
+++ trans.py
@@ -6,17 +6,33 @@ class ScaledDotProductAttention(nn.Module):
     def __init__(self, embed_size):
         super(ScaledDotProductAttention, self).__init__()
         self.embed_size = embed_size
-    def forward(self, Q, K, V, mask=None):
-        scores = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.embed_size, dtype=torch.float32))
+        self.Q = torch.rand(self.embed_size, self.embed_size)
+        self.K = torch.rand(self.embed_size, self.embed_size)
+        self.V = torch.rand(self.embed_size, self.embed_size)
+    def forward(self, tkns: torch.Tensor, mask=None): # tkns dim: ( embed_size, num_tokens)
+        Q = torch.matmul(self.Q, tkns)
+        K = torch.matmul(self.K, tkns)
+        V = torch.matmul(self.V, tkns)
+        scores = torch.matmul(self.Q, self.K.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.embed_size, dtype=torch.float32))
         if mask is not None:
             scores = scores.masked_fill(mask==0, 1e-9)
         
         attn = F.softmax(scores, dim=-1)
-        sftmax = torch.matmul(attn, V)
+        sftmax = torch.matmul(attn, self.V)
         return attn, sftmax
+
+class MultiheadedAttention(nn.Module):
+    def __init__(self, embed_size, num_heads):
+        super(MultiheadedAttention, self).__init__()
+        self.matrix_dim = embed_size // num_heads
+        self.dot_attn = [ScaledDotProductAttention(self.matrix_dim) for _ in range(num_heads)]
+        
+    def forward(self, tkns: torch.Tensor, mask=None):
+        pass
+
 
 
 model = ScaledDotProductAttention(256)
-attn, sftmax = model(torch.rand(32, 32), torch.rand(32, 32), torch.rand(32, 32))
+attn, sftmax = model(torch.rand(256, 10))
 print(sftmax)
 
