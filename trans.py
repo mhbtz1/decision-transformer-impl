f5652de8583b9b84fb15e873079463da2ac2c145
Matthew Habtezgi <mhbtz@mit.edu>
2024-08-04 20:39:33 -0400

    update base transformer code

--- trans.py
+++ trans.py
@@ -1,10 +1,22 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class SelfAttention(torch.nn.Module):
-    def __init__(self):
-        pass
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, embed_size):
+        super(ScaledDotProductAttention, self).__init__()
+        self.embed_size = embed_size
+    def forward(self, Q, K, V, mask=None):
+        scores = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.embed_size, dtype=torch.float32))
+        if mask is not None:
+            scores = scores.masked_fill(mask==0, 1e-9)
+        
+        attn = F.softmax(scores, dim=-1)
+        sftmax = torch.matmul(attn, V)
+        return attn, sftmax
 
-    def forward(x):
-        pass
+
+model = ScaledDotProductAttention(256)
+attn, sftmax = model(torch.rand(32, 32), torch.rand(32, 32), torch.rand(32, 32))
+print(sftmax)
 
